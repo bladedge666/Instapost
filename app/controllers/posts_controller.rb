58a56fe0c6ca1913793ab336a27f1ebbc3f0b5f8
Srijal Joshi <bladedge666@gmail.com>
2016-11-26 22:29:26 +0545

    Use filter before_action :is_owner? on edit & update methods to limit access to edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :is_owner?, only: [:edit, :update]
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
@@ -33,6 +34,10 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def is_owner?
+    redirect_to root_path if Post.find(params[:id]).user != current_user
+  end
 
   def post_params
     params.require(:post).permit(:user_id, :photo, :description)
