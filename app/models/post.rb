f336a65b43858330ada7ee31af150887e867d0e0
Srijal Joshi <bladedge666@gmail.com>
2016-11-26 19:19:19 +0545

    Add strong parameters, relationships betn post & user, and validations to post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id" 
+
+  validates :photo, :description, :user_id, presence: true
   # carrierwave boilerplate
   mount_uploader :photo, PhotoUploader
 
