eb3798b86f8aef9b32085490b7268ec8c509bd42
Srijal Joshi <bladedge666@gmail.com>
2016-11-27 17:15:40 +0545

    Delete comments functionality by checking if the comment's creator is the current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  # before_action :authenticate_user!, only: :create  
+  before_action :if_not_owner_redirect_to_root, only: :destroy #to prevent automated DELETE req from deleting the content
+  before_action :authenticate_user!, only: :create  
 
   def create
     @post = Post.find(params[:post_id])
@@ -12,7 +13,20 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id]) #id here refers to the comment id
+    @comment.destroy
+    redirect_to root_path
+  end
+
   private 
+
+  def if_not_owner_redirect_to_root
+    @comment = Comment.find(params[:id])
+    if @comment.user != current_user
+      redirect_to root_path
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:text, :post_id)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  # validates :text, :user_id, :post_id, presence: true
+  validates :text, :user_id, :post_id, presence: true
 end
