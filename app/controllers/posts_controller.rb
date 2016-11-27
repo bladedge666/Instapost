4efb76115693ae3ca9e792d0c2ed29dee5258d99
Srijal Joshi <bladedge666@gmail.com>
2016-11-27 16:01:10 +0545

    Rename comment*s* controller, posting comments functionality, merge current_user.id to comment_params hash, and comments form on the index page if logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :is_owner?, only: [:edit, :update, :destroy]
+  before_action :if_not_owner_redirect_to_root, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
@@ -46,7 +46,7 @@ class PostsController < ApplicationController
 
   private
 
-  def is_owner?
+  def if_not_owner_redirect_to_root
     redirect_to root_path if Post.find(params[:id]).user != current_user
   end
 
